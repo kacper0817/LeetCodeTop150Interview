01d8f79a45ecf9a7dac3e4a66146cd928957405c
Kacper <k.kacper0871@gmail.com>
2025-04-21 18:16:18 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LeetCodeTop150Interview
+
+go 1.24.2
---
+++ internal/ArrayString/MergeSortedArray.go
@@ -0,0 +1,42 @@
+package arraystring
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	if m == 0 {
+		copy(nums1, nums2)
+	} else if n > 0 {
+		copy(nums1, handleMerge(nums1, m, nums2, n))
+	}
+}
+
+func handleMerge(nums1 []int, m int, nums2 []int, n int) []int {
+	temp := make([]int, m+n)
+	num1Index := 0
+	num2Index := 0
+	i := 0
+
+	for num1Index > -1 || num2Index > -1 {
+		if num1Index == -1 {
+			temp[i] = nums2[num2Index]
+			incrementCounter(&num2Index, n)
+		} else if num2Index == -1 {
+			temp[i] = nums1[num1Index]
+			incrementCounter(&num1Index, m)
+		} else if nums1[num1Index] < nums2[num2Index] {
+			temp[i] = nums1[num1Index]
+			incrementCounter(&num1Index, m)
+		} else {
+			temp[i] = nums2[num2Index]
+			incrementCounter(&num2Index, n)
+		}
+		i++
+	}
+	return temp
+}
+
+func incrementCounter(value *int, maxValue int) {
+	if *value+1 < maxValue {
+		(*value)++
+	} else {
+		*value = -1
+	}
+}
